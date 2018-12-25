660c3eac8fac030f278f29a26afdcdc2ef17fc4f
Christopher Osthues <osthues.christopher@web.de>
2018-12-25 17:14:30 +0100

    Adding tests for lce queries

--- include/lce_query.hpp
+++ include/lce_query.hpp
@@ -17,14 +17,19 @@ namespace lce_query {
  *
  * @tparam variable_t
  */
-template<typename variable_t = var_t>
+//template<typename variable_t = var_t>
 struct vis_node {
 //    variable_t nt;
     size_t pos;
     size_t n_trav;
 
-    vis_node(/*variable_t nt, */size_t pos, size_t n_trav) {
-//        this->nt = nt;
+    vis_node() = default;
+//    vis_node(/*variable_t nt, */size_t pos, size_t n_trav) {
+////        this->nt = nt;
+//        this->pos = pos;
+//        this->n_trav = n_trav;
+//    }
+    vis_node(size_t pos, size_t n_trav) {
         this->pos = pos;
         this->n_trav = n_trav;
     }
@@ -37,11 +42,11 @@ struct vis_node {
  */
 template<typename variable_t = var_t>
 struct lceq {
-//    std::vector<size_t> visited;  // TODO(Chris): replace with bitvector. not important anymore
     /**
      * Maps a non-terminal to its parent, the position in the parent and the number of the traverse.
      */
-    std::unordered_map<variable_t, vis_node<variable_t>> visited;
+//    std::unordered_map<variable_t, vis_node<variable_t>> visited;
+    std::unordered_map<variable_t, vis_node> visited;
     std::stack<variable_t> parents;
 //    std::stack<vis_node<variable_t>> stack;
 
@@ -67,7 +72,7 @@ size_t lce_query(const rlslp<variable_t, terminal_count_t>& rlslp, size_t i, siz
  * @return
  */
 template<typename variable_t = var_t, typename terminal_count_t = term_t>
-size_t find_next(rlslp<variable_t, terminal_count_t>& rlslp, size_t i, size_t j, variable_t nt_i, variable_t nt_j, lceq<variable_t>& i_visited, lceq<variable_t>& j_visited, size_t traverse) {
+size_t find_next(const rlslp<variable_t, terminal_count_t>& rlslp, size_t i, size_t j, variable_t nt_i, variable_t nt_j, lceq<variable_t>& i_visited, lceq<variable_t>& j_visited, size_t traverse) {
     auto parent_i = nt_i;
     auto comp_i_len = rlslp.len(nt_i) - i;
     while (i + comp_i_len >= rlslp.len(parent_i) && parent_i != rlslp.root + rlslp.terminals) {
@@ -78,7 +83,7 @@ size_t find_next(rlslp<variable_t, terminal_count_t>& rlslp, size_t i, size_t j,
         i = i_visited.visited[parent_i].pos;
     }
     i_visited.visited[parent_i].pos = i + comp_i_len;
-    i_visited.visited[parent_i].traverse = traverse;
+    i_visited.visited[parent_i].n_trav = traverse;
 
     auto parent_j = nt_j;
     auto comp_j_len = rlslp.len(nt_j) - j;
@@ -90,7 +95,7 @@ size_t find_next(rlslp<variable_t, terminal_count_t>& rlslp, size_t i, size_t j,
         j = j_visited.visited[parent_j].pos;
     }
     j_visited.visited[parent_j].pos = j + comp_j_len;
-    j_visited.visited[parent_j].traverse = traverse;
+    j_visited.visited[parent_j].n_trav = traverse;
 
     if ((parent_i == rlslp.root + rlslp.terminals && i + comp_i_len >= rlslp.len(parent_i)) || (parent_j == rlslp.root + rlslp.terminals && j + comp_j_len >= rlslp.len(parent_j))) {
         return 0;
@@ -109,7 +114,7 @@ size_t find_next(rlslp<variable_t, terminal_count_t>& rlslp, size_t i, size_t j,
  * @param nt
  */
 template<typename variable_t = var_t, typename terminal_count_t = term_t>
-inline void subtree(rlslp<variable_t, terminal_count_t>& rlslp, size_t& pos, variable_t& nt) {
+inline void subtree(const rlslp<variable_t, terminal_count_t>& rlslp, size_t& pos, variable_t& nt) {
     if (nt >= rlslp.terminals) {
         auto nt_index = nt - rlslp.terminals;  // compute index of production
         auto left_child = rlslp[nt_index].first();
@@ -167,7 +172,8 @@ size_t lce_query(const rlslp<variable_t, terminal_count_t>& rlslp, size_t i, siz
         subtree(rlslp, pos_j, child_j);
 
         if (nt_i != child_i) {
-            i_visited.visited[child_i] = vis_node<variable_t>(/*nt_i, */pos_i, traverse);
+            i_visited.visited[child_i] = vis_node{/*nt_i, */pos_i, traverse};
+//            i_visited.visited.insert(child_i, pos_i, traverse);
             i_visited.parents.push(nt_i);
 //            if (child_i >= rlslp.terminals) {
 //                i_visited.visited[child_i - rlslp.terminals] = traverse;
@@ -175,7 +181,8 @@ size_t lce_query(const rlslp<variable_t, terminal_count_t>& rlslp, size_t i, siz
 //            i_visited.stack.push(child_i, pos_i);//, traverse);
         }
         if (nt_j != child_j) {
-            j_visited.visited[child_j] = vis_node<variable_t>(/*nt_j, */pos_j, traverse);
+            j_visited.visited[child_j] = vis_node{/*nt_j, */pos_j, traverse};
+//            j_visited.visited.insert(child_j, pos_j, traverse);//[child_j] = vis_node<variable_t>(/*nt_j, */pos_j, traverse);
             j_visited.parents.push(nt_j);
 //            if (child_j >= rlslp.terminals) {
 //                j_visited.visited[child_j - rlslp.terminals] = traverse;
@@ -188,10 +195,10 @@ size_t lce_query(const rlslp<variable_t, terminal_count_t>& rlslp, size_t i, siz
         auto found_i = j_visited.visited.find(child_i);
         if (rlslp.is_block(child_i) && rlslp.is_block(child_j) && rlslp[child_i - rlslp.terminals].first() == rlslp[child_j - rlslp.terminals].first() && pos_i % rlslp.len(rlslp[child_i - rlslp.terminals].first()) == pos_j % rlslp.len(rlslp[child_j - rlslp.terminals].first())) {  // access of len is save because terminals cannot derive blocks
             return std::min(rlslp.len(child_i) - pos_i, rlslp.len(child_j) - pos_j) + find_next(rlslp, pos_i, pos_j, child_i, child_j, i_visited, j_visited, traverse + 1);
-        } else if (found_i != j_visited.visited.end() && (*found_i).pos == pos_i && (*found_i).traverse == traverse) {
+        } else if (found_i != j_visited.visited.end() && (*found_i).second.pos == pos_i && (*found_i).second.n_trav == traverse) {
             // TODO(Chris): check if j_visited[child_i] exists
             return rlslp.len(child_i) - pos_i + find_next(rlslp, pos_i, pos_j, child_i, child_j, i_visited, j_visited, traverse + 1);
-        } else if (found_j != j_visited.visited.end() && (*found_j).pos == pos_j && (*found_j).traverse == traverse) {
+        } else if (found_j != j_visited.visited.end() && (*found_j).second.pos == pos_j && (*found_j).second.n_trav == traverse) {
             // TODO(Chris): check if i_visited[child_j] exists
             return rlslp.len(child_j) - pos_j + find_next(rlslp, pos_i, pos_j, child_i, child_j, i_visited, j_visited, traverse + 1);
         } else if (child_i < rlslp.terminals && child_j < rlslp.terminals) {
@@ -205,6 +212,7 @@ size_t lce_query(const rlslp<variable_t, terminal_count_t>& rlslp, size_t i, siz
         }
     }
 }
+
 
 /**
  * @brief
@@ -228,8 +236,8 @@ size_t lce_query(const rlslp<variable_t, terminal_count_t>& rlslp, size_t i, siz
         return rlslp[rlslp.root].len;
     }
 
-    lceq<variable_t> i_visited();
-    lceq<variable_t> j_visited();
+    lceq<variable_t> i_visited;
+    lceq<variable_t> j_visited;
 //    lceq<variable_t> i_visited(rlslp.size());
 //    lceq<variable_t> j_visited(rlslp.size());
 //    i_visited.stack.push(rlslp.root + rlslp.terminals, i);//, 1);
@@ -237,7 +245,8 @@ size_t lce_query(const rlslp<variable_t, terminal_count_t>& rlslp, size_t i, siz
 //    i_visited.visited[rlslp.root] = 1;
 //    j_visited.visited[rlslp.root] = 1;
 
-    return lce_query(rlslp, i, j, rlslp.root + rlslp.terminals, rlslp.root + rlslp.terminals, i_visited, j_visited, 1);
+    return lce_query<variable_t, terminal_count_t>(rlslp, i, j, static_cast<variable_t>(rlslp.root + rlslp.terminals),
+                     static_cast<variable_t>(rlslp.root + rlslp.terminals), i_visited, j_visited, 1);
 }
 
 }  // namespace lce_query
--- test/lce_query_test.cpp
+++ test/lce_query_test.cpp
@@ -1,7 +1,204 @@
 #include <gtest/gtest.h>
 
+#include "defs.hpp"
 #include "lce_query.hpp"
+#include "rlslp.hpp"
+#include "parallel_recompression.hpp"
 
-TEST(lce_query, empty) {
+using namespace recomp;
 
+typedef recompression<var_t, term_t>::text_t text_t;
+
+TEST(lcequery, lcequery_empty) {
+    text_t text;
+    recompression<var_t, term_t> recomp;
+    rlslp<var_t, term_t> rlslp;
+    recomp.recomp(text, rlslp);
+
+    auto lceq = lce_query::lce_query<var_t, term_t>(rlslp, 0, 10);
+    ASSERT_EQ(0, lceq);
+}
+
+TEST(lcequery, lcequery_zero) {
+    text_t text{2, 1, 2, 1, 4, 4, 4, 1, 3, 3, 2, 3, 1, 1, 4, 1, 3, 3, 3, 4, 1, 3, 3, 2, 3, 1, 1, 4, 1, 3, 2, 1};
+    recompression<var_t, term_t> recomp;
+    rlslp<var_t, term_t> rlslp;
+    recomp.recomp(text, rlslp);
+
+    auto lceq = lce_query::lce_query(rlslp, 1, 0);
+    ASSERT_EQ(0, lceq);
+}
+
+TEST(lcequery, lcequery_max_len_i) {
+    text_t text = {2, 1, 2, 1, 4, 4, 4, 1, 3, 3, 2, 3, 1, 1, 4, 1, 3, 3, 3, 4, 1, 3, 3, 2, 3, 1, 1, 4, 1, 3, 2, 1};
+    recompression<var_t, term_t> recomp;
+    rlslp<var_t, term_t> rlslp;
+    recomp.recomp(text, rlslp);
+
+    auto lceq = lce_query::lce_query(rlslp, 32, 5);
+    ASSERT_EQ(0, lceq);
+}
+
+TEST(lcequery, lcequery_max_len_j) {
+    text_t text = {2, 1, 2, 1, 4, 4, 4, 1, 3, 3, 2, 3, 1, 1, 4, 1, 3, 3, 3, 4, 1, 3, 3, 2, 3, 1, 1, 4, 1, 3, 2, 1};
+    recompression<var_t, term_t> recomp;
+    rlslp<var_t, term_t> rlslp;
+    recomp.recomp(text, rlslp);
+
+    auto lceq = lce_query::lce_query(rlslp, 5, 32);
+    ASSERT_EQ(0, lceq);
+}
+
+TEST(lcequery, lcequery) {
+    text_t text = {2, 1, 2, 1, 4, 4, 4, 1, 3, 3, 2, 3, 1, 1, 4, 1, 3, 3, 3, 4, 1, 3, 3, 2, 3, 1, 1, 4, 1, 3, 2, 1};
+    recompression<var_t, term_t> recomp;
+    rlslp<var_t, term_t> rlslp;
+    recomp.recomp(text, rlslp);
+
+    auto lceq = lce_query::lce_query(rlslp, 6, 14);
+//    ASSERT_EQ(1, lceq);
+    ASSERT_EQ(4, lceq);
+}
+
+TEST(lcequery, lcequery_long) {
+    text_t text = {2, 1, 2, 1, 4, 4, 4, 1, 3, 3, 2, 3, 1, 1, 4, 1, 3, 3, 3, 4, 1, 3, 3, 2, 3, 1, 1, 4, 1, 3, 2, 1};
+    recompression<var_t, term_t> recomp;
+    rlslp<var_t, term_t> rlslp;
+    recomp.recomp(text, rlslp);
+
+    auto lceq = lce_query::lce_query(rlslp, 6, 19);
+//    ASSERT_EQ(1, lceq);
+    ASSERT_EQ(11, lceq);
+}
+
+TEST(lcequery, lcequery_asso) {
+    text_t text = {2, 1, 2, 1, 4, 4, 4, 1, 3, 3, 2, 3, 1, 1, 4, 1, 3, 3, 3, 4, 1, 3, 3, 2, 3, 1, 1, 4, 1, 3, 2, 1};
+    recompression<var_t, term_t> recomp;
+    rlslp<var_t, term_t> rlslp;
+    recomp.recomp(text, rlslp);
+
+    auto lceq_ij = lce_query::lce_query(rlslp, 6, 14);
+    auto lceq_ji = lce_query::lce_query(rlslp, 14, 6);
+    ASSERT_EQ(4, lceq_ji);
+//    ASSERT_EQ(1, lceq_ji);
+    ASSERT_EQ(lceq_ij, lceq_ji);
+}
+
+TEST(lcequery, lcequery_complete_text) {
+    text_t text = {2, 1, 2, 1, 4, 4, 4, 1, 3, 3, 2, 3, 1, 1, 4, 1, 3, 3, 3, 4, 1, 3, 3, 2, 3, 1, 1, 4, 1, 3, 2, 1};
+    auto t_size = text.size();
+    recompression<var_t, term_t> recomp;
+    rlslp<var_t, term_t> rlslp;
+    recomp.recomp(text, rlslp);
+
+    auto lceq = lce_query::lce_query(rlslp, 0, 0);
+    ASSERT_EQ(t_size, lceq);
+}
+
+TEST(lcequery, lcequery_same_position) {
+    text_t text = {2, 1, 2, 1, 4, 4, 4, 1, 3, 3, 2, 3, 1, 1, 4, 1, 3, 3, 3, 4, 1, 3, 3, 2, 3, 1, 1, 4, 1, 3, 2, 1};
+    auto t_size = text.size();
+    recompression<var_t, term_t> recomp;
+    rlslp<var_t, term_t> rlslp;
+    recomp.recomp(text, rlslp);
+
+    auto lceq = lce_query::lce_query(rlslp, 10, 10);
+    ASSERT_EQ(t_size - 10, lceq);
+}
+
+TEST(lcequery, lcequery_compressed_begin) {
+    text_t text = {2, 1, 2, 1, 4, 4, 4, 1, 3, 3, 2, 3, 1, 1, 4, 1, 3, 3, 3, 4, 1, 3, 3, 2, 3, 1, 1, 4, 1, 3, 2, 1};
+    recompression<var_t, term_t> recomp;
+    rlslp<var_t, term_t> rlslp;
+    recomp.recomp(text, rlslp);
+
+    auto lceq = lce_query::lce_query(rlslp, 2, 0);
+    ASSERT_EQ(2, lceq);
+}
+
+TEST(lcequery, lcequery_compressed_within) {
+    text_t text = {2, 1, 2, 1, 4, 4, 4, 1, 3, 3, 2, 3, 1, 1, 4, 1, 3, 3, 3, 4, 1, 3, 3, 2, 3, 1, 1, 4, 1, 3, 2, 1};
+    recompression<var_t, term_t> recomp;
+    rlslp<var_t, term_t> rlslp;
+    recomp.recomp(text, rlslp);
+
+    auto lceq = lce_query::lce_query(rlslp, 3, 1);
+    ASSERT_EQ(1, lceq);
+}
+
+TEST(lcequery, lcequery_compressed_diff) {
+    text_t text = {2, 1, 2, 1, 4, 4, 4, 1, 3, 3, 2, 3, 1, 1, 4, 1, 3, 3, 3, 4, 1, 3, 3, 2, 3, 1, 1, 4, 1, 3, 2, 1};
+    recompression<var_t, term_t> recomp;
+    rlslp<var_t, term_t> rlslp;
+    recomp.recomp(text, rlslp);
+
+    auto lceq = lce_query::lce_query(rlslp, 2, 1);
+    ASSERT_EQ(0, lceq);
+}
+
+TEST(lcequery, lcequery_compressed_in_tree) {
+    text_t text = {2, 1, 2, 1, 4, 4, 4, 1, 3, 3, 2, 3, 1, 1, 4, 1, 3, 3, 3, 4, 1, 3, 3, 2, 3, 1, 1, 4, 1, 3, 2, 1};
+    recompression<var_t, term_t> recomp;
+    rlslp<var_t, term_t> rlslp;
+    recomp.recomp(text, rlslp);
+
+    auto lceq = lce_query::lce_query(rlslp, 8, 21);
+    ASSERT_EQ(9, lceq);
+
+    lceq = lce_query::lce_query(rlslp, 10, 23);
+    ASSERT_EQ(7, lceq);
+}
+
+TEST(lcequery, lcequery_different_block_same_symbol) {
+    text_t text = {2, 1, 2, 1, 4, 4, 4, 1, 3, 3, 2, 3, 1, 1, 4, 1, 3, 3, 3, 4, 1, 3, 3, 2, 3, 1, 1, 4, 1, 3, 2, 1, 2, 1,
+                   2, 1};
+    recompression<var_t, term_t> recomp;
+    rlslp<var_t, term_t> rlslp;
+    recomp.recomp(text, rlslp);
+
+    auto lceq = lce_query::lce_query(rlslp, 1, 31);
+    ASSERT_EQ(3, lceq);
+
+    lceq = lce_query::lce_query(rlslp, 1, 35);
+    ASSERT_EQ(1, lceq);
+
+    lceq = lce_query::lce_query(rlslp, 33, 35);
+    ASSERT_EQ(1, lceq);
+
+    lceq = lce_query::lce_query(rlslp, 31, 33);
+    ASSERT_EQ(3, lceq);
+}
+
+size_t lce_query_naive(size_t i, size_t j, text_t &text) {
+    size_t lceq = 0;
+    auto iter_i = text.begin();
+    std::advance(iter_i, i);
+    auto iter_j = text.begin();
+    std::advance(iter_j, j);
+    while (iter_i != text.end() && iter_j != text.end() && (*iter_i) == (*iter_j)) {
+        lceq++;
+        ++iter_i;
+        ++iter_j;
+    }
+    return lceq;
+}
+
+TEST(lcequery, lcequery_complete) {
+    text_t text = {2, 1, 2, 1, 4, 4, 4, 1, 3, 3, 2, 3, 1, 1, 4, 1, 3, 3, 3, 4, 1, 3, 3, 2, 3, 1, 1, 4, 1, 3, 2, 1};
+    text_t text_naive = {2, 1, 2, 1, 4, 4, 4, 1, 3, 3, 2, 3, 1, 1, 4, 1, 3, 3, 3, 4, 1, 3, 3, 2, 3, 1, 1, 4, 1, 3, 2, 1};
+    recompression<var_t, term_t> recomp;
+    rlslp<var_t, term_t> rlslp;
+    recomp.recomp(text, rlslp);
+
+    for (size_t i = 0; i < text.size(); ++i) {
+        for (size_t j = 0; j < text.size(); ++j) {
+//            std::cout << "recompression_list" << std::endl;
+            auto lceq = lce_query::lce_query(rlslp, i, j);
+//            std::cout << "recompression_list finished" << std::endl;
+//            std::cout << "naive" << std::endl;
+            size_t naive = lce_query_naive(i, j, text_naive);
+//            std::cout << "naive finished" << std::endl;
+            ASSERT_EQ(naive, lceq);
+        }
+    }
 }
