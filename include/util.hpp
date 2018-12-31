a212922d9c4ae7cca738a8615d17d5b6f52667e1
Christopher Osthues <osthues.christopher@web.de>
2018-12-31 19:37:07 +0100

    Add read file methods

--- include/util.hpp
+++ include/util.hpp
@@ -2,6 +2,7 @@
 #pragma once
 
 #include <fstream>
+#include <iterator>
 #include <string>
 #include <unordered_map>
 #include <utility>
@@ -14,8 +15,6 @@
 namespace recomp {
 namespace util {
 
-//void read_file(const std::string& file_name, text_t& text);
-
 template<typename text_t>
 std::string text_vector_to_string(const text_t& text) {
     std::stringstream text_stream;
@@ -23,6 +22,63 @@ std::string text_vector_to_string(const text_t& text) {
         text_stream << c << " ";
     }
     return text_stream.str();
+}
+
+/**
+ * @brief Faster loading of a file but with more memory consumption.
+ * @tparam text_t The type of text
+ * @param[in] file_name The file name
+ * @param[out] text The text to be read in
+ */
+template<typename text_t>
+void read_file_fast(const std::string& file_name, text_t& text) {
+    LOG(INFO) << "Reading file: " << file_name;
+    std::ifstream ifs(file_name.c_str(), std::ios::in | std::ios::binary | std::ios::ate);
+    auto file_size = ifs.tellg();
+    if (file_size < 0) {
+        return;
+    }
+    ifs.seekg(0, std::ios::beg);
+
+    text.resize(static_cast<size_t>(file_size), '\0');
+    std::vector<char> in(file_size);
+    ifs.read((char*)in.data(), file_size);
+    for (size_t i = 0; i < in.size(); ++i) {
+        text[i] = static_cast<typename text_t::value_type>(in[i]);
+    }
+    ifs.close();
+    LOG(INFO) << "Read " << file_size << " bytes";
+    LOG(INFO) << "Finished reading file";
+//    LOG(INFO) << "Text fast: " << text_vector_to_string(text);
+}
+
+template<typename text_t>
+void read_file(const std::string& file_name, text_t& text) {
+    LOG(INFO) << "Reading file: " << file_name;
+    std::ifstream ifs(file_name.c_str(), std::ios::in | std::ios::binary | std::ios::ate);
+    auto file_size = ifs.tellg();
+    if (file_size < 0) {
+        LOG(ERROR) << "Failed to read file " << file_name;
+        return;
+    }
+    ifs.seekg(0, std::ios::beg);
+
+    text.resize(static_cast<size_t>(file_size), '\0');
+
+    char c;
+    auto index = 0;
+    while (ifs.get(c)) {
+        text[index++] = static_cast<typename text_t::value_type>(c);
+    }
+    ifs.close();
+
+    LOG(INFO) << "Read " << file_size << " bytes";
+    LOG(INFO) << "Finished reading file";
+//    LOG(INFO) << "Text: " << text_vector_to_string(text);
+
+//    std::ifstream ifs(file_name.c_str());
+//    std::istream_iterator<recomp::var_t> input(ifs);
+//    std::copy(input, std::istream_iterator<recomp::var_t>(), std::back_inserter(text));
 }
 
 template<typename block_t, typename variable_t>
