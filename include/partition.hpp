6cc93e5c5c6121d5b8a4c949b9f772728c0b38aa
Christopher Osthues <osthues.christopher@web.de>
2019-01-22 19:14:58 +0100

    Clean up in partition

--- include/partition.hpp
+++ include/partition.hpp
@@ -13,10 +13,6 @@
 #include "defs.hpp"
 #include "util.hpp"
 
-//#ifndef THREAD_COUNT
-//#define THREAD_COUNT std::thread::hardware_concurrency()
-//#endif
-
 namespace recomp {
 
 namespace parallel {
@@ -24,11 +20,10 @@ namespace parallel {
  * @brief Computes a partitioning of the symbol in the text.
  *
  * @param multiset[in] The adjacency list of the text
- * @param alphabet[in] The effective alphabet
  * @param partition[out] The partition
  */
 template<typename multiset_t/*, typename alphabet_t*/, typename partition_t>
-inline void compute_partition(const multiset_t& multiset, /*const alphabet_t& alphabet,*/
+inline void compute_partition(const multiset_t& multiset,
                               partition_t& partition,
                               const size_t cores = std::thread::hardware_concurrency()) {
 #ifdef BENCH
@@ -139,8 +134,6 @@ inline void compute_partition(const multiset_t& multiset, /*const alphabet_t& al
     const auto timeSpan = endTime - startTime;
     std::cout << " partition=" << std::chrono::duration_cast<std::chrono::milliseconds>(timeSpan).count();
 #endif
-//    DLOG(INFO) << "Time for computing partition: "
-//               << std::chrono::duration_cast<std::chrono::milliseconds>(timeSpan).count() << "[ms]";
 }
 
 
@@ -148,70 +141,43 @@ inline void compute_partition(const multiset_t& multiset, /*const alphabet_t& al
  * @brief Computes a partitioning of the symbol in the text.
  *
  * @param multiset[in] The adjacency list of the text
- * @param alphabet[in] The effective alphabet
  * @param partition[out] The partition
  */
 template<typename variable_t, typename multiset_t, typename alphabet_t, typename partition_t>
 inline void compute_partition_full_parallel(const multiset_t& multiset,
-//                                            const alphabet_t& alphabet,
                                                   partition_t& partition,
                                                   const size_t cores = std::thread::hardware_concurrency()) {
 #ifdef BENCH
     const auto startTime = std::chrono::system_clock::now();
 #endif
+
     alphabet_t alphabet(partition.size());
-//#pragma omp parallel num_threads(cores)
-//    {
-        auto partition_iter = partition.begin();
-//#pragma omp for schedule(static)
-        for (size_t i = 0; i < partition.size(); ++i) {
-
-            alphabet[i] = (*partition_iter).first;
-            ++partition_iter;
+    auto partition_iter = partition.begin();
+#pragma omp parallel for schedule(static) num_threads(cores)
+    for (size_t i = 0; i < partition.size(); ++i) {
+        if (partition_iter == partition.begin()) {
+            std::advance(partition_iter, i);
         }
-//    }
+        alphabet[i] = (*partition_iter).first;
+        ++partition_iter;
+    }
     ips4o::parallel::sort(alphabet.begin(), alphabet.end());
-//    partitioned_radix_sort(alphabet);
-//    __gnu_parallel::sort(alphabet.begin(), alphabet.end(), __gnu_parallel::multiway_mergesort_tag());
-//    DLOG(INFO) << util::text_vector_to_string(alphabet);
-
-//    std::unordered_map<variable_t, std::shared_timed_mutex> mutexes;
-//    std::cout << "Alphabet: " << util::text_vector_to_string(alphabet) << std::endl;
 
     std::vector<std::shared_timed_mutex> mutexes(alphabet.size());
     std::unordered_map<variable_t, size_t> mapping;
-//    std::unordered_map<variable_t, bool> locks;
 #pragma omp parallel for schedule(static) num_threads(cores)
     for (size_t i = 0; i < alphabet.size(); ++i) {
 #pragma omp critical
         {
             mapping[alphabet[i]] = i;
-//            locks[alphabet[i]] = false;
         }
     }
 
-//    for (const auto& m: mapping) {
-//        std::cout << m.first << ", " << m.second << std::endl;
-//    }
-
-//        std::cout << "Locks: " << std::endl;
-//        for (const auto& lock : locks) {
-//            std::cout << lock.first << ", " << lock.second << std::endl;
-//        }
-
-//        std::cout << std::endl;
     // TODO(Chris): parallelize histgram
     std::vector<size_t> hist(alphabet.size() + 1, 0);
     for (size_t i = 0; i < multiset.size(); ++i) {
-//            std::cout << std::get<0>(multiset[i]) << " ";
         hist[mapping[std::get<0>(multiset[i])]]++;
     }
-//    std::cout << std::endl;
-
-//    for (size_t i = 0; i < multiset.size(); ++i) {
-//        std::cout << std::get<1>(multiset[i]) << " ";
-//    }
-//    std::cout << std::endl;
 
     std::vector<size_t> starts;
     starts.push_back(0);
@@ -222,27 +188,9 @@ inline void compute_partition_full_parallel(const multiset_t& multiset,
         }
     }
 
-//        std::cout << "starts: " << starts[0] << ", ";
     for (size_t i = 1; i < starts.size(); ++i) {
-////            std::cout << starts[i] << ", ";
         starts[i] += starts[i - 1];
     }
-//        std::cout << std::endl;
-//
-//        std::cout << "starts: ";
-//        for (size_t i = 0; i < starts.size(); ++i) {
-//            std::cout << starts[i] << ", ";
-//        }
-//        std::cout << std::endl;
-//
-//
-//
-////        std::unordered_map<variable_t, omp_lock_t> locks{};
-////        for (size_t i = 0; i < alphabet.size(); ++i) {
-////            omp_lock_t lock;
-////            locks[alphabet[i]] = lock;
-////            omp_init_lock(&locks[alphabet[i]]);
-////        }
 
     std::vector<size_t> bounds;
 #pragma omp parallel num_threads(cores)
@@ -262,13 +210,9 @@ inline void compute_partition_full_parallel(const multiset_t& multiset,
                 n_threads = starts.size();
             }
             bounds[0] = 0;
-//                std::cout << "bounds: 0, ";
             for (size_t i = 1; i < n_threads; ++i) {
                 bounds[i] = bounds[i-1] + step;
-//                    std::cout << bounds[i] << ", ";
             }
-//                bounds[n_threads] = starts.size() - 1;
-//                std::cout << bounds[omp_get_num_threads()] << ", " << std::endl;
         }
 
         for (size_t i = bounds[thread_id]; i < bounds[thread_id + 1]; ++i) {
@@ -277,31 +221,8 @@ inline void compute_partition_full_parallel(const multiset_t& multiset,
             mutexes[mapping[std::get<0>(multiset[starts[i]])]].lock();
 //#pragma omp critical
 //            {std::cout << "Locked write " << std::get<0>(multiset[starts[i]]) << " by thread " << thread_id << std::endl;}
-
-
-
-// #pragma omp critical
-//                {
-//                    std::cout << "Locking: " << std::get<0>(multiset[starts[i]]) << " by thread " << thread_id
-//                              << " i: " << i << std::endl;
-//                }
-//            auto found = mutexes.find(std::get<0>(multiset[starts[i]]));
-//            (*found).second.lock();
-//            std::unique_lock<std::shared_timed_mutex> lock((*found).second, std::defer_lock);
-//            std::lock(lock);
-//            (*found).second = !(*found).second;
-//                locks[std::get<0>(multiset[starts[i]])] = true;
-
-//                omp_set_lock(&locks[std::get<0>(multiset[starts[i]])]);
         }
 #pragma omp barrier
-// #pragma omp single
-//            {
-//                std::cout << "All locks aquired" << std::endl;
-//                for (const auto& lock : locks) {
-//                    std::cout << lock.first << ", " << lock.second << std::endl;
-//                }
-//            }
 
         int l_count = 0;
         int r_count = 0;
@@ -312,38 +233,6 @@ inline void compute_partition_full_parallel(const multiset_t& multiset,
             while (index < starts[i + 1]) {
 //#pragma omp critical
 //                {std::cout << "Index " << index << " to " << starts[i+1] << " by thread " << thread_id << " char " << mapping[std::get<0>(multiset[index])] << std::endl;}
-
-//// #pragma omp critical
-////                    {
-////                        std::cout << "Test for: " << std::get<1>(multiset[index]) << " by thread " << thread_id
-////                                  << std::endl;
-////                    }
-//                bool locked = false;
-//#pragma omp critical
-//                {
-//                    locked = locks[std::get<1>(multiset[index])];
-//                }
-//                while (locked) {
-//#pragma omp critical
-//                    {
-//                        locked = locks[std::get<1>(multiset[index])];
-//                    }
-//                }
-//                    while (!omp_test_lock(&locks[std::get<1>(multiset[index])])) {
-//                        std::cout << "Waiting thread: " << thread_id << std::endl;
-//                    }
-//                bool check = false;
-//#pragma omp critical
-//                {
-//                    check = partition[std::get<1>(multiset[index])];
-//                }
-//                if (check) {
-//                    r_count++;
-//                } else {
-//                    l_count++;
-//                }
-//                index++;
-
 
 //#pragma omp critical
 //                {std::cout << "Locking shared " << std::get<1>(multiset[index]) << " by thread " << thread_id << std::endl;}
@@ -371,48 +260,13 @@ inline void compute_partition_full_parallel(const multiset_t& multiset,
 //#pragma omp critical
 //            {std::cout << "Unlocked write " << std::get<0>(multiset[index - 1]) << " by thread " << thread_id << std::endl;}
 
-//            (*write_found).second.unlock();
-//#pragma omp critical
-//            {
-//                partition[std::get<0>(multiset[index - 1])] = l_count > r_count;
-//            }
             l_count = 0;
             r_count = 0;
-// #pragma omp critical
-//                {
-//                    std::cout << "Releasing: " << std::get<0>(multiset[index - 1]) << " by thread " << thread_id
-//                              << std::endl;
-//                }
-//#pragma omp critical
-//            {
-//                locks[std::get<0>(multiset[index - 1])] = false;
-//            }
-//                omp_unset_lock(&locks[std::get<0>(multiset[index - 1])]);
         }
-
-//#pragma omp barrier
     }
-//        delete[] bounds;
+    mutexes.resize(0);
+    mutexes.shrink_to_fit();
 
-//        for (size_t i = 0; i < alphabet.size(); ++i) {
-//            omp_destroy_lock(&locks[alphabet[i]]);
-//        }
-
-//        for (size_t i = 0; i < multiset.size(); ++i) {
-//            while (j < alphabet.size() && std::get<0>(multiset[i]) > alphabet[j]) {
-//                partition[alphabet[j]] = l_count > r_count;
-////                DLOG(INFO) << "Setting " << alphabet[j] << " to " << (l_count > r_count);
-//                j++;
-//                l_count = 0;
-//                r_count = 0;
-//            }
-//            if (partition[std::get<1>(multiset[i])]) {
-//                r_count++;
-//            } else {
-//                l_count++;
-//            }
-//        }
-//        partition[alphabet[j]] = l_count > r_count;
 #ifdef BENCH
     const auto endTimePar = std::chrono::system_clock::now();
     const auto timeSpanPar = endTimePar - startTime;
@@ -473,8 +327,6 @@ inline void compute_partition_full_parallel(const multiset_t& multiset,
     const auto timeSpan = endTime - startTime;
     std::cout << " partition=" << std::chrono::duration_cast<std::chrono::milliseconds>(timeSpan).count();
 #endif
-//    DLOG(INFO) << "Time for computing partition: "
-//               << std::chrono::duration_cast<std::chrono::milliseconds>(timeSpan).count() << "[ms]";
 }
 
 
