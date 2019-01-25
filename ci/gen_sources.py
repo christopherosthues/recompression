e95a91808a0246ae3edc66a4a36de5828190d1ef
Christopher Osthues <osthues.christopher@web.de>
2019-01-25 18:51:31 +0100

    Add script for source file generation

---
+++ ci/gen_sources.py
@@ -0,0 +1,15 @@
+import os
+import os.path
+
+dir = os.listdir()
+
+print(dir)
+
+for root, sub_dirs, files in os.walk("."):
+    for file in files:
+        if file.endswith(".hpp"):
+            r = root[1:] if root == "." else root[2:]
+            os.makedirs("../src/" + r + "/", exist_ok=True)
+            source_file = open("../src/" + r + "/" + file[:len(file)-3] + "cpp", 'w')
+            source_file.write('#include "' + file + '"')
+            source_file.close()
