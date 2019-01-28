d62114c49b233367b5976c10221cb7f82842ea4d
Christopher Osthues <osthues.christopher@web.de>
2019-01-28 14:55:43 +0100

    Download script for test data

--- bench/download_data.sh
+++ bench/download_data.sh
@@ -1,13 +1,18 @@
-$text_collec="http://pizzachili.dcc.uchile.cl/texts/"
-§files="code/sources xml/dblp.xml dna/dna protein/proteins nlang/english"
+text_collec="http://pizzachili.dcc.uchile.cl/texts/"
+files="code/sources xml/dblp.xml dna/dna protein/proteins nlang/english"
+file_names="sources dblp.xml dna proteins english"
 
-$rep_corpus="http://pizzachili.dcc.uchile.cl/repcorpus/real/"
-$rep_files="Escherichia_Coli coreutils einstein.de.txt einstein.en.txt kernel world_leaders"
+rep_corpus="http://pizzachili.dcc.uchile.cl/repcorpus/real/"
+rep_files="Escherichia_Coli coreutils einstein.de.txt einstein.en.txt kernel world_leaders"
 
 for file in $rep_files; do
     wget "$rep_corpus$file.gz"
 done
+for file_name in $file_names; do
+    gunzip $file_name
+done
 
 for file in $files; do
     wget "$text_collec$file.gz"
+    gunzip $file
 done
