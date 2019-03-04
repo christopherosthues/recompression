b36d1097a47aca223b2856f69433cd1238df5bcf
Christopher Osthues <osthues.christopher@web.de>
2019-03-04 15:29:27 +0100

    rule sorter almost correct

--- include/coders/rlslp_rule_sorter.hpp
+++ include/coders/rlslp_rule_sorter.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-//#include <iostream>
+#include <iostream>
 #include <queue>
 #include <stack>
 #include <thread>
@@ -55,7 +55,8 @@ inline void sort_rlslp_rules(rlslp<variable_t, terminal_count_t>& rlslp, size_t 
 
     size_t new_index = 0;
     std::vector<variable_t> renamed(rlslp.size());
-    std::queue<variable_t> block_queue;
+    std::stack<variable_t> stack;
+    std::stack<variable_t> block_stack;
     // Rename variable until a block is found on the path
     while (!queue.empty()) {
         auto var = queue.front();
@@ -64,14 +65,18 @@ inline void sort_rlslp_rules(rlslp<variable_t, terminal_count_t>& rlslp, size_t 
         if (first[var] != var) {
             auto child = first[var];
             if (rlslp.is_block(child)) {
-                block_queue.push(child);
+//                block_queue.push(child);
+                stack.push(child);
+                block_stack.push(child);
             } else {
                 queue.push(child);
             }
             while (next[child] != child) {
                 child = next[child];
                 if (rlslp.is_block(child)) {
-                    block_queue.push(child);
+//                    block_queue.push(child);
+                    stack.push(child);
+                    block_stack.push(child);
                 } else {
                     queue.push(child);
                 }
@@ -83,36 +88,90 @@ inline void sort_rlslp_rules(rlslp<variable_t, terminal_count_t>& rlslp, size_t 
         }
     }
 //    std::cout << "Renamed til blocks" << std::endl;
+    std::queue<variable_t> block_queue;
+    while (!stack.empty()) {
+        auto var = stack.top();
+        stack.pop();
+        block_queue.push(var);
+    }
 
-    std::stack<variable_t> block_stack;
-    // Identify all blocks
+    std::stack<variable_t> tmp_block_stack;
+    std::stack<variable_t> tmp_stack;
     while (!block_queue.empty()) {
         auto var = block_queue.front();
         block_queue.pop();
-        if (rlslp.is_block(var)) {
-            block_stack.push(var);
-        }
+//        if (rlslp.is_block(var)) {
+//            stack.push(var);
+//        }
 
         if (first[var] != var) {
             auto child = first[var];
-            block_queue.push(child);
+            if (rlslp.is_block(child)) {
+                tmp_block_stack.push(child);
+            }
+            tmp_stack.push(child);
             while (next[child] != child) {
                 child = next[child];
-                block_queue.push(child);
+                if (rlslp.is_block(child)) {
+                    tmp_block_stack.push(child);
+                }
+                tmp_stack.push(child);
+            }
+            while (!tmp_block_stack.empty()) {
+                stack.push(tmp_block_stack.top());
+                tmp_block_stack.pop();
+            }
+            while (!tmp_stack.empty()) {
+                block_queue.push(tmp_stack.top());
+                tmp_stack.pop();
             }
         }
     }
-//    std::cout << "blocks identified" << std::endl;
 
-    // Rename blocks
+    while (!stack.empty()) {
+        block_stack.push(stack.top());
+        stack.pop();
+    }
+
     auto block_idx = rlslp.size() - 1;
-    new_index = rlslp.blocks - 1;
     while (!block_stack.empty()) {
         auto var = block_stack.top();
         block_queue.push(var);
+//        std::cout << var << std::endl;
         block_stack.pop();
         renamed[var - rlslp.terminals] = block_idx--;
     }
+
+
+    // Identify all blocks
+//    while (!block_queue.empty()) {
+//        auto var = block_queue.front();
+//        block_queue.pop();
+//        if (rlslp.is_block(var)) {
+//            block_stack.push(var);
+//        }
+//
+//        if (first[var] != var) {
+//            auto child = first[var];
+//            block_queue.push(child);
+//            while (next[child] != child) {
+//                child = next[child];
+//                block_queue.push(child);
+//            }
+//        }
+//    }
+//    std::cout << "blocks identified" << std::endl;
+
+    // Rename blocks
+//    auto block_idx = rlslp.size() - 1;
+
+    new_index = rlslp.blocks - 1;
+//    while (!block_stack.empty()) {
+//        auto var = block_stack.top();
+//        block_queue.push(var);
+//        block_stack.pop();
+//        renamed[var - rlslp.terminals] = block_idx--;
+//    }
 //    std::cout << "blocks renamed" << std::endl;
 
     while (!block_queue.empty()) {
