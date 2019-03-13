7ec9f857233f41db86b82b2be5425ab855d3a985
Christopher Osthues <osthues.christopher@web.de>
2019-03-13 23:03:53 +0100

    Add benchmark script for long lces using rmqs to find them first

--- bench/long_lce_query_bench.cpp
+++ bench/long_lce_query_bench.cpp
@@ -15,7 +15,7 @@
 
 int main(int argc, char *argv[]) {
     if (argc < 8) {
-        std::cerr << "./bench_lce_query [path] [file_name(s)] [recomp (parallel_lp) | naive | prezza | rmq] [repeats] [begin] [end] [step] [coder (fixed | plain | sorted)] [rlslp_path] [prefix]" << std::endl;
+        std::cerr << "./bench_long_lce_query [path] [file_name(s)] [recomp (parallel_lp) | naive | prezza | rmq] [repeats] [begin] [end] [step] [coder (fixed | plain | sorted)] [rlslp_path] [prefix]" << std::endl;
         return -1;
     }
 
@@ -26,8 +26,6 @@ int main(int argc, char *argv[]) {
     recomp::util::split(argv[3], " ", algos);
 
     size_t repeats = (size_t)recomp::util::str_to_int(argv[4]);
-
-    std::srand(0);
 
     size_t begin = (size_t) recomp::util::str_to_int(argv[5]);
     size_t end = (size_t) recomp::util::str_to_int(argv[6]);
@@ -49,10 +47,6 @@ int main(int argc, char *argv[]) {
 
     for (size_t k = 0; k < files.size(); ++k) {
         std::string file_name = argv[1] + files[k];
-        size_t file_size = recomp::util::file_size_in_bytes(file_name);
-        if (prefix > 0) {
-            file_size = std::min(file_size, prefix);
-        }
 
         lce::lceDataStructure prezza;
         lce::buildLCEDataStructure(&prezza, file_name);
@@ -111,10 +105,42 @@ int main(int argc, char *argv[]) {
         sdsl::rmq_succinct_sct<> rmq(&lcp);
 
         for (size_t accesses = begin; accesses <= end; accesses += step) {
-            std::vector<size_t> indices(accesses);
-            for (size_t i = 0; i < accesses; ++i) {
-                indices[i] = recomp::util::random_number(file_size);
+            std::vector<size_t> indices(accesses + accesses);
+            std::vector<size_t> idx(plain_text.size());
+            for (size_t n = 0; n < idx.size(); ++n) {
+                idx[n] = n;
             }
+
+            auto sort_cond = [&](const size_t i, const size_t j) {
+                return rmq(j, j) < rmq(i, i);
+            };
+
+            ips4o::parallel::sort(idx.begin(), idx.end(), sort_cond, 4);
+
+//            size_t max = 0;
+//            for (size_t i = 0; i < plain_text.size(); ++i) {
+//                auto rmq_val = rmq(i, i);
+//                if (rmq_val > max) {
+//                    max = rmq_val;
+//                }
+//            }
+//            std::cout << "Max lce: " << max << std::endl;
+
+            for (size_t i = 0, n = 0; i < indices.size(); i += 2) {
+                indices[i] = sa[idx[n]];
+                if (idx[n] == 0) {
+                    indices[i + 1] = indices[i];
+                } else {
+                    indices[i + 1] = sa[idx[n] - 1];
+                }
+//                std::cout << indices[i] << ", " << indices[i + 1] << std::endl;
+                n++;
+                if (n >= idx.size()) {
+                    n = 0;
+                }
+            }
+            idx.resize(0);
+            idx.shrink_to_fit();
 
             for (size_t repeat = 0; repeat < repeats; ++repeat) {
                 std::vector<size_t> lces(algos.size(), 0);
@@ -126,7 +152,7 @@ int main(int argc, char *argv[]) {
                     if (algo == "recomp") {
                         const auto startTime = recomp::timer::now();
                         for (size_t i = 0; i < accesses; ++i) {
-                            lces[l] += recomp::lce_query::lce_query(rlslp, indices[i], indices[(i + 1) % accesses]);
+                            lces[l] += recomp::lce_query::lce_query(rlslp, indices[i], indices[i + 1]);
                         }
                         const auto endTime = recomp::timer::now();
                         const auto timeSpan = endTime - startTime;
@@ -138,8 +164,7 @@ int main(int argc, char *argv[]) {
                     } else if (algo == "naive") {
                         const auto startTime = recomp::timer::now();
                         for (size_t i = 0; i < accesses; ++i) {
-                            lces[l] += recomp::lce_query::lce_query_naive(plain_text, indices[i],
-                                                                          indices[(i + 1) % accesses]);
+                            lces[l] += recomp::lce_query::lce_query_naive(plain_text, indices[i], indices[i + 1]);
                         }
                         const auto endTime = recomp::timer::now();
                         const auto timeSpan = endTime - startTime;
@@ -150,7 +175,7 @@ int main(int argc, char *argv[]) {
                     } else if (algo == "prezza") {
                         const auto startTime = recomp::timer::now();
                         for (size_t i = 0; i < accesses; ++i) {
-                            lces[l] += lce::fastlce(&prezza, indices[i], indices[(i + 1) % accesses]);
+                            lces[l] += lce::fastlce(&prezza, indices[i], indices[i + 1]);
                         }
                         const auto endTime = recomp::timer::now();
                         const auto timeSpan = endTime - startTime;
@@ -162,16 +187,12 @@ int main(int argc, char *argv[]) {
                         const auto startTime = recomp::timer::now();
                         for (size_t i = 0; i < accesses; ++i) {
                             auto isa_i = sa.isa[indices[i]];
-                            auto isa_i1 = sa.isa[indices[(i + 1) % accesses]];
+                            auto isa_i1 = sa.isa[indices[i + 1]];
                             auto left = std::min(isa_i, isa_i1);
                             auto right = std::max(isa_i, isa_i1);
                             if (left == right) {
                                 lces[l] += plain_text.size() - indices[i];
                             } else {
-                                //                            std::cout << "l: " << left << ", r: " << right << std::endl;
-//                            std::cout << "rmq: " << rmq(std::min(left, right), std::max(left, right)) << std::endl;
-//                            std::cout << "lcp size: " << lcp.size() << std::endl;
-//                            std::cout << "lcp: " << lcp[rmq(std::min(left, right), std::max(l, right))] << std::endl;
                                 lces[l] += lcp[rmq(left + 1, right)];
                             }
                         }
@@ -195,115 +216,5 @@ int main(int argc, char *argv[]) {
             }
         }
     }
-
-    return 0;
-
-//    if (argc < 7) {
-//        std::cerr << "lce_query_bench [path] [file_name(s)] [recomp (parallel_ls) | naive | rmq] [repeats] i j" << std::endl;
-//        return -1;
-//    }
-//
-//    std::vector<std::string> files;
-//    recomp::util::split(argv[2], " ", files);
-//
-//    std::vector<std::string> algos;
-//    recomp::util::split(argv[3], " ", algos);
-//
-//    size_t repeats = (size_t)recomp::util::str_to_int(argv[4]);
-//
-//    auto i = (size_t)recomp::util::str_to_int(argv[5]);
-//    auto j = (size_t)recomp::util::str_to_int(argv[6]);
-//
-//    for (size_t k = 0; k < files.size(); ++k) {
-//        for (size_t repeat = 0; repeat < repeats; ++repeat) {
-//            for (size_t l = 0; l < algos.size(); ++l) {
-//                std::cout << "Iteration: " << repeat << std::endl;
-//                std::string algo = algos[l];
-//                std::cout << "Using algo " << algo << std::endl;
-//
-//                std::string file_name = argv[1] + files[k];
-//
-//                size_t pos = file_name.find_last_of('/');
-//                std::string dataset;
-//                if (pos != std::string::npos) {
-//                    dataset = file_name.substr(pos + 1);
-//                } else {
-//                    dataset = file_name;
-//                }
-//
-//                recomp::util::replace_all(dataset, "_", "\\_");
-//
-//                if (algo == "recomp") {
-//                    typedef recomp::parallel::recompression_order_ls<recomp::var_t, recomp::term_t>::text_t text_t;
-//                    text_t text;
-//                    recomp::util::read_file(file_name, text);
-//
-//                    recomp::rlslp<recomp::var_t, recomp::term_t> rlslp;
-//                    recomp::parallel::recompression_order_ls<recomp::var_t, recomp::term_t> recompression;
-//                    recompression.recomp(text, rlslp, recomp::CHAR_ALPHABET, 4);
-//
-//                    const auto startTime = recomp::timer::now();
-//
-//                    auto lce = recomp::lce_query::lce_query(rlslp, i, j);
-//
-//                    const auto endTime = recomp::timer::now();
-//                    const auto timeSpan = endTime - startTime;
-//                    std::cout << "RESULT algo=recompression lce=" << lce << " i=" << i << " j=" << j << " time="
-//                              << std::chrono::duration_cast<std::chrono::milliseconds>(timeSpan).count() << std::endl;
-//                } else if (algo == "naive") {
-//                    typedef recomp::parallel::recompression_order_ls<recomp::var_t, recomp::term_t>::text_t text_t;
-//                    text_t text;
-//                    recomp::util::read_file(file_name, text);
-//
-////                    size_t sum_lce = 0;
-////                    size_t max_i = 0;
-////                    size_t max_j = 0;
-////                    size_t max_lce = 0;
-////                    size_t sum = 0;
-////                    for (size_t i = 0; i < text.size(); ++i) {
-////                        for (size_t j = i + 1; j < text.size(); ++j) {
-//                            const auto startTime = recomp::timer::now();
-//                            size_t lce = recomp::lce_query::lce_query_naive(text, i, j);
-//                            const auto endTime = recomp::timer::now();
-//                            const auto timeSpan = endTime - startTime;
-//                            std::cout << "RESULT algo=naive lce=" << lce << " i=" << i << " j=" << j << " time="
-//                                      << std::chrono::duration_cast<std::chrono::milliseconds>(timeSpan).count()
-//                                      << std::endl;
-////                            sum_lce += lce;
-////                            sum++;
-////                            if (max_lce < lce) {
-////                                max_lce = lce;
-////                                max_i = i;
-////                                max_j = j;
-////                            }
-////                        }
-////                    }
-////                    std::cout << "RESULT algo=naive_lce max_lce=" << max_lce << " max_i=" << max_i << " max_j="
-////                              << max_j << " lces=" << sum_lce << " sum=" << sum << std::endl;
-//                } else if (algo == "rmq") {
-//                    // TODO(Chris): RMQ on lcp array
-//                    // compute first A, then H, then ISA and then RMQ data structure on H
-//                    // LCE(i,j) = RMQ_H(ISA[i], ISA[j])
-////                    sdsl::csa_wt<> csa;
-//                    sdsl::csa_sada<> csa;
-//                    sdsl::construct(csa, file_name, 1);
-////                    sdsl::lcp
-////                    sdsl::construct()
-//                    sdsl::rmq_succinct_sada<> rmq;
-////                    rmq = sdsl::rmq_succinct_sada();
-//                    const auto startTime = recomp::timer::now();
-//                    auto lce = 0;
-//                    const auto endTime = recomp::timer::now();
-//                    const auto timeSpan = endTime - startTime;
-//                    std::cout << "RESULT algo=rmq lce=" << lce << " i=" << i << " j=" << j << " time="
-//                              << std::chrono::duration_cast<std::chrono::milliseconds>(timeSpan).count() << std::endl;
-//                } else {
-//                    std::cout << "No such algorithm '" << algo << "'." << std::endl;
-//                    return -1;
-//                }
-//            }
-//        }
-//    }
-
     return 0;
 }
