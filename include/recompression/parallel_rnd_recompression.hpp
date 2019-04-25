cfaad67f4854dfd6d73542106272fde7adf98cd1
Christopher Osthues <osthues.christopher@web.de>
2019-04-25 17:22:00 +0200

    Random generator

--- include/recompression/parallel_rnd_recompression.hpp
+++ include/recompression/parallel_rnd_recompression.hpp
@@ -289,13 +289,16 @@ class parallel_rnd_recompression : public parallel_lp_recompression<variable_t, 
         const auto startTimePar = recomp::timer::now();
 #endif
 
-        std::random_device rd;
-        std::mt19937 gen(rd());
-        std::uniform_int_distribution<uint8_t> distribution(0, 1);
+//        std::random_device rd;
+//        std::mt19937 gen(rd());
+//        std::uniform_int_distribution<uint8_t> distribution(0, 1);
         partition[0] = 0;  // ensure, that minimum one symbol is in the left partition and one in the right
         partition[partition.size() - 1] = 1;
 #pragma omp parallel num_threads(this->cores)
         {
+            std::random_device rd;
+            std::mt19937 gen(rd());
+            std::uniform_int_distribution<uint8_t> distribution(0, 1);
 #pragma omp for schedule(static)
             for (size_t i = 1; i < partition.size() - 1; ++i) {
                 partition[i] = distribution(gen);
